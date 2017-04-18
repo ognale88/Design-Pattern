26d9116d745ae5627cf6d5c8158d31db99e0857f
Elango Thanumalayan <ognale88@gmail.com>
2017-04-18 11:09:55 +0530

    Uploaded 2 Files to Proxy Folder

---
+++ Proxy/RealSubject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DesignPatterns.Proxy
+{
+    class RealSubject : Subject
+    {
+        public override void Request(string Value)
+        {
+            MessageBox.Show("Called Request method in RealSubject Class Value : "+ Value,"Real Subject Class");
+        }
+    }
+}
