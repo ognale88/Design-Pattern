bc4511ca996211bf0a47b201e0883eb7102b30b9
Elango Thanumalayan <ognale88@gmail.com>
2017-04-18 11:07:47 +0530

    3 Files uploaded to Observer Folder
    IInverstor,cs contains the interface for the method declaration.
    Investor.cs contains the Interface IInvestor implementation along with the own properties
    Stock.cs contains the properties.

---
+++ Observer/Investor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DesignPatterns.Observer
+{
+    class Investor : IInvestor
+    {
+        private string _name;
+        private Stock _stock;
+        public Investor(string name)
+        {
+            this._name = name;
+        }
+
+        public void Update(Stock Member)
+        {
+            MessageBox.Show("Notified to " + _name + " for " + Member.Symbol + " changed to " + Member.Price);
+        }
+        public Stock Stock
+        {
+            get { return _stock; }
+            set { _stock = value; }
+        }
+    }
+}
